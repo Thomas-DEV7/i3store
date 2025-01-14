21bec59b1b2f09828b4004970a9b87931e717cfc
Thomas Bastos <thomas.felip16@gmail.com>
2025-01-13 23:15:06 -0300

    feat: create product controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $featuredProducts = Product::take(5)->get(); // Produtos em destaque (os 5 primeiros)
+        $products = Product::all(); // Todos os produtos
+        return view('products.index', compact('featuredProducts', 'products'));
+    }
+}
